1c99ec4c6fe368db212747ae094b4d9e5abdca0f
Gabe Kopley <gabe@coshx.com>
2014-09-19 17:20:46 -0700

    Add EventSource+CORS test

--- web/controllers/test_controller.ex
+++ web/controllers/test_controller.ex
@@ -1,5 +1,3 @@
-require IEx
-
 defmodule CorstestServer.TestController do
   use Phoenix.Controller
 
@@ -8,6 +6,27 @@ defmodule CorstestServer.TestController do
     {a, ""} = Integer.parse(conn.params["a"])
     {b, ""} = Integer.parse(conn.params["b"])
     json conn, JSON.encode!(%{sum: a+b})
+  end
+
+  def eventsource(conn, _params) do
+    conn = Plug.Conn.put_resp_header(conn, "access-control-allow-origin", "*")
+    conn = Plug.Conn.put_resp_content_type(conn, "text/event-stream")
+
+    conn = Plug.Conn.send_chunked(conn, 200)
+
+    {a, ""} = Integer.parse(conn.params["a"])
+    {b, ""} = Integer.parse(conn.params["b"])
+
+    data = "event: a\ndata: #{a}\n\n"
+    Plug.Conn.chunk(conn, data)
+
+    data = "event: b\ndata: #{b}\n\n"
+    Plug.Conn.chunk(conn, data)
+
+    data = "event: sum\ndata: #{a+b}\n\n"
+    Plug.Conn.chunk(conn, data)
+
+    conn
   end
 
   def not_found(conn, _params) do
--- web/router.ex
+++ web/router.ex
@@ -3,6 +3,7 @@ defmodule CorstestServer.Router do
   use Phoenix.Router.Socket, mount: "/tests/websockets"
 
   get "/tests/simple_cors", CorstestServer.TestController, :simple_cors
+  get "/tests/eventsource", CorstestServer.TestController, :eventsource
   channel "test_channel", CorstestServer.TestChannel
 
 end
