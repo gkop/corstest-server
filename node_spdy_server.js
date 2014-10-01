2a41ddad926286db1d37fdb18eca675a8c92cd1f
Gabe Kopley <gabe@coshx.com>
2014-10-01 00:21:35 +0000

    Make eventsource work in SPDY server

--- node_spdy_server.js
+++ node_spdy_server.js
@@ -14,11 +14,48 @@ var options = {
 };
 
 var server = spdy.createServer(options, function(req, res) {
-  res.writeHead(200, {"Access-Control-Allow-Origin": "*"});
-  var parsed_url = url.parse(req.url, true);
-  var sum = parseInt(parsed_url.query.a) + parseInt(parsed_url.query.b);
-  var response = {spdy: (req.socket._httpMessage.isSpdy === true), sum: sum}; 
-  res.end(JSON.stringify(response));
+  var sum, headers, a, b;
+  if (req.method === "OPTIONS") {
+    headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "x-a, x-b"
+    };
+    res.writeHead(200, headers);
+    res.end();
+  }
+  else {
+    var headers = {
+      "Access-Control-Allow-Origin": "*"
+    };
+    var parsed_url = url.parse(req.url, true);
+    if (parsed_url.query.a && parsed_url.query.b) {
+      // params passed in query string
+      a = parsed_url.query.a;
+      b = parsed_url.query.b;
+    }
+    else {
+      // params passed in headers
+      a = req.headers['x-a'];
+      b = req.headers['x-b'];
+    }
+
+    sum = parseInt(a) + parseInt(b);
+
+    if (req.url.match(/eventsource/)) {
+      headers["Content-Type"] = "text/event-stream; charset=utf-8";
+      res.writeHead(200, headers);
+      res.write("event: a\ndata: "+a+"\n\n");
+      res.write("event: b\ndata: "+b+"\n\n");
+      res.write("event: sum\ndata: "+sum+"\n\n");
+    }
+    else {
+      var response = {
+        spdy: (req.socket._httpMessage.isSpdy === true), sum: sum
+      }; 
+      res.writeHead(200, headers);
+      res.end(JSON.stringify(response));
+    }
+  }
 });
 
 server.listen(4002);
